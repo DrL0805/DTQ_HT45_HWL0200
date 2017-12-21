bd8f84ffadcfa50b49872e79646f231412993444
DrL0805 <1052061675@qq.com>
2017-12-21 23:55:28 +0800

    1）休眠指令重新增加__WFI(),因为去掉__WFI()指令后发现有时按键无效，对休眠指令的控制把握不够；2）去除进入休眠的条件，nrf_esb_is_idel()，增加这个条件，虽然逻辑更加严谨（若处于发送模式，能保证发送缓存数据全部发送完全才进入休眠），但会让开机平均功耗增加约80uA

--- user/main.c
+++ user/main.c
@@ -48,9 +48,9 @@ int main (void)
 	
 	while(true)
 	{
-		drTIMER_EventHandler();
+		drTIMER_EventHandler();	
 		TIMER_EventHandler();	
-		switch(POWER.SysState)
+		switch(POWER.SysState)	
 		{
 			case SYS_ON:				
 				RADIO_RxDataHandler();	
@@ -83,9 +83,10 @@ int main (void)
 		drERR_ErrHandler();
 		
 		// 若无外设数据进行处理，CPU休眠
-		if(!get_rx_fifo_count() && !LCD.DATA.RefreshFlg && nrf_esb_is_idle())
+		if(!get_rx_fifo_count() && !LCD.DATA.RefreshFlg)
 		{
 			__WFE();
+			__WFI();
 		}
 	}
 }
